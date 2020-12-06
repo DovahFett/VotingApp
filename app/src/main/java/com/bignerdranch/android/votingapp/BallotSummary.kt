21b96875bbe3742fcdc50968ee902efd30b4cf14
DovahFett <oblivionslegend@gmail.com>
2020-12-05 21:12:12 -0500

    Improved formatting on BallotSummary.kt. Added functionality to btnBallotSummary.

--- app/src/main/java/com/bignerdranch/android/votingapp/BallotSummary.kt
+++ app/src/main/java/com/bignerdranch/android/votingapp/BallotSummary.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.votingapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.ballot_summary.*
 
@@ -13,13 +15,68 @@ class BallotSummary : AppCompatActivity()
         setContentView(R.layout.ballot_summary)
 
         var ballot = intent.getSerializableExtra("ElectionDetails") as Ballot?
+        var user = intent.getSerializableExtra("User") as User?
         //Creates an array to hold the choices made in the previously accessed ballot.
         var ballotChoices = ArrayList<String>()
         ballotChoices.add(intent.getSerializableExtra("Choice 1") as String)
 
         if (ballot != null)
         {
-            lblChoice1.text = getString(R.string.lblChoice1, ballot.positionName, ballotChoices[0]);
+            if(ballotChoices.size == 1)
+            {
+                txtPosition1.text = getString(R.string.txtPosition1, "Position:", ballot.positionName)
+                txtChoice1.text = getString(R.string.txtChoice1, "Choice:", ballotChoices[0])
+                txtPosition2.text = getString(R.string.txtPosition2, "", "")
+                txtChoice2.text = getString(R.string.txtChoice2, "", "")
+                txtPosition3.text = getString(R.string.txtPosition3, "", "")
+                txtChoice3.text = getString(R.string.txtChoice3, "", "")
+                txtPosition4.text = getString(R.string.txtPosition4, "", "")
+                txtChoice4.text = getString(R.string.txtChoice4, "", "")
+            }
+            if(ballotChoices.size == 2)
+            {
+                txtPosition1.text = getString(R.string.txtPosition1, "Position:", ballot.positionName)
+                txtChoice1.text = getString(R.string.txtChoice1, "Choice:", ballotChoices[0])
+                txtPosition2.text = getString(R.string.txtPosition2, "Position:", ballot.positionName)
+                txtChoice2.text = getString(R.string.txtChoice2, "Choice:", ballotChoices[1])
+                txtPosition3.text = getString(R.string.txtPosition3, "", "")
+                txtChoice3.text = getString(R.string.txtChoice3, "", "")
+                txtPosition4.text = getString(R.string.txtPosition4, "", "")
+                txtChoice4.text = getString(R.string.txtChoice4, "", "")
+            }
+            if(ballotChoices.size == 3)
+            {
+                txtPosition1.text = getString(R.string.txtPosition1, "Position:", ballot.positionName)
+                txtChoice1.text = getString(R.string.txtChoice1, "Choice:", ballotChoices[0])
+                txtPosition2.text = getString(R.string.txtPosition2, "Position:", ballot.positionName)
+                txtChoice2.text = getString(R.string.txtChoice2, "Choice:", ballotChoices[1])
+                txtPosition3.text = getString(R.string.txtPosition3, "Position:", ballot.positionName)
+                txtChoice3.text = getString(R.string.txtChoice3, "Choice:", ballotChoices[2])
+                txtPosition4.text = getString(R.string.txtPosition4, "", "")
+                txtChoice4.text = getString(R.string.txtChoice4, "", "")
+            }
+            if(ballotChoices.size == 4)
+            {
+                txtPosition1.text = getString(R.string.txtPosition1, "Position:", ballot.positionName)
+                txtChoice1.text = getString(R.string.txtChoice1, "Choice:", ballotChoices[0])
+                txtPosition2.text = getString(R.string.txtPosition2, "Position:", ballot.positionName)
+                txtChoice2.text = getString(R.string.txtChoice2, "Choice:", ballotChoices[1])
+                txtPosition3.text = getString(R.string.txtPosition3, "Position:", ballot.positionName)
+                txtChoice3.text = getString(R.string.txtChoice3, "Choice:", ballotChoices[2])
+                txtPosition4.text = getString(R.string.txtPosition4, "Position:", ballot.positionName)
+                txtChoice4.text = getString(R.string.txtChoice4, "Choice:", ballotChoices[3])
+            }
+
+            btnBallotSummary.setOnClickListener{
+                ballot.status = "Closed"
+                val intent = Intent(this, BallotList::class.java)
+                intent.putExtra("User", user)//Pass the user object
+                intent.putExtra("ElectionDetails", ballot)
+                Toast.makeText(this, "Confirmed", Toast.LENGTH_LONG).show()
+                startActivity(intent)
+            }
         }
+
+
     }
 }
